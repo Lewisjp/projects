92e904eaedcfde303a16c1fb7fe83dd8d85aa3b1
LewisJPaul <Flatiron@new-host-3.home>
2013-11-14 12:29:49 -0500

    removed the ruby that was hanging out

--- todos/loops/times_five/times_five.js
+++ todos/loops/times_five/times_five.js
@@ -1,8 +1,3 @@
-=begin 
- 
-The 'times_five' folder should contain a file called times_five.js and times_five.rb. Each file should contain code which, when ran, will print each number, 1-100, multiplied by 5.
- 
-=end
 
 
 for (var i = 1; i < 101; i = i * 5 ) {
