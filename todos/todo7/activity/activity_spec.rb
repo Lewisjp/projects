bfc4269577559872f8811889edcb541e6de97687
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2013-11-20 14:00:48 -0500

    added rspec

--- todos/todo7/activity/activity_spec.rb
+++ todos/todo7/activity/activity_spec.rb
@@ -1,52 +1,21 @@
 require './activity'
 
-def my_collect(array)
-	i = 0
-	new_array = Array.new
-	while i < array.length
-		yield array[i]
 
-			if array[i] == "paper"
-			new_array << true 
-			else 
-			new_array << false 
-			end		
+describe Array, "#my_collect" do
 
-		i += 1
+	it "should return an array that has boolean values." do
+	expect(["rock", "paper", "scissors"].my_collect { |hand| hand == "paper" }).to eql([false, true, false])
 	end
-	new_array
-end
-
-c_array = ["rock", "paper", "scissors"] 
-puts my_collect(c_array) { |hand| hand == "paper" }
-
-describe "#my_collect"  do
-	it "should return an array that has boolean values."
-	expect()
-	
 end
 
 
 
-def my_select(array)
-	i = 0
-	new_array = Array.new
-	while i < array.length 
-		yield array[i]
+describe Array, "#my_select" do
 
-			if array[i] == "monster"
-				new_array << "monster"
-			end
-
-
-		i += 1
+	it "should return an array with all the items that equal our desired object." do
+	expect([ "monster", "monster", "red bull", "monster", "red bull"].my_select { |drink| drink == "monster"}.to eql(["monster", "monster","monster"])
 	end
-	new_array
 end
-
-c_array = [ "monster", "monster", "red bull", "monster", "red bull"]
-puts my_select(c_array) {|drink| drink == "monster" }
-
 =begin
 CHALLENGE:
 Implement these methods as instance methods on the array class.
