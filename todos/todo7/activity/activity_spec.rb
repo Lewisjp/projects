c98c9d24166a75cc224a02bef6d5b5bdc1dbb107
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2013-11-20 14:05:07 -0500

    rspec update

--- todos/todo7/activity/activity_spec.rb
+++ todos/todo7/activity/activity_spec.rb
@@ -13,7 +13,7 @@ end
 describe Array, "#my_select" do
 
 	it "should return an array with all the items that equal our desired object." do
-	expect([ "monster", "monster", "red bull", "monster", "red bull"].my_select { |drink| drink == "monster"}.to eql(["monster", "monster","monster"])
+	expect([ "monster", "monster", "red bull", "monster", "red bull"].my_select { |drink| drink == "monster"}).to eql(["monster", "monster","monster"])
 	end
 end
 =begin
