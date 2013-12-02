4e7eb3699d60af218d890921d63d5f967fead0e3
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2013-12-02 14:23:36 -0500

    working

--- todos/todo12/todo12.rb
+++ todos/todo12/todo12.rb
@@ -12,23 +12,7 @@ Write a method that will take a tweet, search it for words that you can substitu
 "at" becomes "@" 
 "and" becomes "&"
 ```
-
-and here is the list of tweets:
-
-1. "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-2. "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?",
-"I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
-3. "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
-
-**Objectives**
-
-1. Write a method to shorten a string based on the allowed substitutes
-2. Write a method that iterates over the list of tweets, shortens them, and prints the results to the screen
-
-`hint: you'll need a way to look up each word and see if it has a substitution`
-
-Now the client has come back to you. They love the program, but they noticed that even tweets that are under 140 characters are being shortened. Now they want you to only shorten the tweet if it's too long.
-
+ 
 **Objectives**
 
 1. Modify your program to only do the substitutions if the tweet is longer than 140 characters
@@ -37,28 +21,97 @@ Things are going great, but you're noticing that some tweets are too long still,
 
 **Objectives**
 
-1. Modify your program to truncate the tweet to 140 characters if it's still too long after substitution
+# 1. Modify your program to truncate the tweet to 140 characters if it's still too long after substitution
 =end
 
-
+# 1. Write a method to shorten a string based on the allowed substitutes
 def twitterfy(message)
 	answer = Array.new
 	check_words = message.split(" ")
-	puts check_words
 	check_words.each do |word|
-		word.gsub "to", "2"
-		word.gsub "two", "2"
-		word.gsub "too", "2"
-		word.gsub "for", "4"
-		word.gsub "four", "4"
-		word.gsub "be", "b"
-		word.gsub "you", "u"
-		word.gsub "at", "@"
-		word.gsub "and", "&"
-		answer << word 
+		word.gsub! "to", "2"
+		word.gsub! "two", "2"
+		word.gsub! "too", "2"
+		word.gsub! "for", "4"
+		word.gsub! "four", "4"
+		word.gsub! "be", "b"
+		word.gsub! "you", "u"
+		word.gsub! "at", "@"
+		word.gsub! "and", "&"
+		answer << word
 	end
-	puts answer 
+	answer.join(" ").inspect
 end
 
-test =  "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-twitterfy(test)
+# test =  "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+# twitterfy(test)
+
+
+
+# 2. Write a method that iterates over the list of tweets, shortens them, and prints the results to the screen
+
+# `hint: you'll need a way to look up each word and see if it has a substitution`
+
+def twitterfy_all(arr)
+	new_arr = Array.new
+	arr.each do |twitter|
+		twitter = twitterfy(twitter)
+		new_arr << twitter
+	end
+	puts new_arr
+end
+
+
+# 3. Now the client has come back to you. They love the program, but they noticed that even tweets that are under 140 characters are being shortened. Now they want you to only shorten the tweet if it's too long.
+
+def shorten_long_tweets(tweet)
+	new_arr = Array.new
+	
+	if tweet.class == String
+		if tweet.length > 140
+			return twitterfy(tweet)
+		else 
+			return tweet
+		end
+	elsif tweet.class == Array
+		tweet.each do |twitter|
+			if twitter.length > 140
+				twitter = twitterfy(twitter)
+			end
+			new_arr << twitter
+		end
+		return new_arr
+	else
+		puts "Invalid input.  Enter String or Array of Strings."
+	end
+end
+
+#tests the 3rd criteria with an array of strings
+
+
+
+tweet1 = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+#146 characters 
+
+tweet2 = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"
+#121 characters
+
+tweet3 = "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
+#160 characters
+
+tweet4 = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
+#230 characters
+
+tweets = Array.new
+tweets << tweet1 << tweet2 << tweet3 << tweet4 
+
+
+puts shorten_long_tweets(tweets)
+
+
+
+
+
+
+
+
