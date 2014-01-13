33267f44c9c4247a3bb98b625749c150a441a898
LewisJPaul <Flatiron@Jeremys-MacBook-Air.local>
2014-01-13 09:32:30 -0500

    initial

---
+++ todos/todo34/todo34/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo34::Application.config.secret_token = 'c738f42f32daf46b5887322399b5c1211cb6de84a2e2bea666b0761f5e731817e220fad5ca0e168daafc1867147bb9fe3413b46c694591a662dec98b3f3259a6'
