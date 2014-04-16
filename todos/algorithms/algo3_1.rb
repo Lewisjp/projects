69c3cc25a63ef0973a145b003df9bd50311aa098
LewisJPaul <lewis.jp@gmail.com>
2014-04-15 23:04:05 -0400

    initial placeholder

---
+++ todos/algorithms/algo3_1.rb
@@ -0,0 +1,13 @@
+=begin 
+
+Chapter 3 Growth of Functions
+	- asymptotic notation
+	- standard notation and common functions
+
+Chapter 4 Recurrences
+	- substitution method
+	- recursion-tree method
+	- master method
+	- proof of master theorem
+
+=end 
---
+++ todos/todo12/todo12_spec.rb
@@ -0,0 +1,7 @@
+require_relative 'todo12'
+
+test =  "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
+describe "twitterfy" do
+	
+end
